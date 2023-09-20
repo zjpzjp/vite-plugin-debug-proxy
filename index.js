59bcb3910796a5dff553d3752b5cb0d9c4c46063
zjpzjp <zjpzjp@foxmail.com>
2023-09-20 17:37:40 +0800

    -

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const proxy = httpProxy.createProxyServer({
     secure: false
 })
 
-module.exports = () => ({
+module.exports = (optios) => ({
     name: 'debug-proxy',
     configureServer(server) {
+        optios = Object.assign({
+            default: "https://www.baidu.com",
+            changeOrigin: true,
+        }, options)
         server.middlewares.use((req, res, next) => {
             let u = new URL(req.url, 'http://localhost')
             if (u.pathname.startsWith('/api/')) {
                 let urlObj = new URL(req.headers.referer)
                 var debug = urlObj.search.slice(1).match(new RegExp('(^|&)debug=([^&]*)(&|$)', 'i'))
-                debug = debug ? debug[2] : 'http://www.baidu.com'
+                debug = debug ? debug[2] : options.default
                 req.url = req.url.replace(/^\/api/, '')
                 proxy.web(req, res, {
-                    changeOrigin: true,
+                    changeOrigin: optios.changeOrigin,
                     target: debug
                 })
                 proxy.on('error', (err) => {
